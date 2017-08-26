d47c12c254d0bd63bc6a64a7b4dacb10b92bf7e3
BernhardFuchs <bernhard.fuchs57@gmail.com>
2017-08-26 15:34:27 +0200

    #3 parse single and multi-line value

--- src/main/java/ParseCK2FileService.java
+++ src/main/java/ParseCK2FileService.java
@@ -1,27 +1,87 @@
 import org.apache.commons.lang3.StringUtils;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by fuchs on 25/08/2017.
  */
 public class ParseCK2FileService {
-    public String readFile(String filename) throws IOException {
-        return readFile(filename, null);
+
+    public static final String TAB_PREFIX = "\t";
+    public static final String EQUAL_SUFFIX = "=";
+
+    public Map<String, Map<String, String>> readMultiLineValue(String filename, String searchTerm) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(filename));
+        Map<String, Map<String, String>> valueMap = new HashMap<>();
+        String line;
+        do {
+             line = bufferedReader.readLine();
+             if (StringUtils.startsWith(line, TAB_PREFIX + searchTerm + EQUAL_SUFFIX)){
+                 valueMap = extractValueMap(line, bufferedReader);
+                 break;
+             }
+        } while(line != null);
+
+        return valueMap;
     }
 
-    public String readFile(String filename, String searchTerm) throws IOException {
+    private Map<String, Map<String, String>> extractValueMap(String line, BufferedReader bufferedReader) throws IOException {
+        Map<String, String> subMap = new HashMap<>();
+        String subLine;
+        do {
+            subLine = bufferedReader.readLine();
+            if (isNotOpenBracket(subLine) && isNotClosingBracket(subLine)){
+                String subKey = extractKey(subLine);
+                String subValue = extractValue(subLine);
+                subMap.put(subKey, subValue);
+            }
+        } while (isNotClosingBracket(subLine));
+
+        Map<String, Map<String, String>> rootMap = new HashMap<>();
+        String rootKey = stripSpecialChars(StringUtils.remove(line, EQUAL_SUFFIX));
+        rootMap.put(rootKey, subMap);
+
+        return rootMap;
+    }
+
+    private boolean isNotClosingBracket(String subLine) {
+        return !subLine.equals(TAB_PREFIX + "}");
+    }
+
+    private boolean isNotOpenBracket(String subLine) {
+        return !subLine.equals(TAB_PREFIX + "{");
+    }
+
+    private String extractKey(String subLine) {
+        String[] splitLine = StringUtils.split(subLine, "=");
+        String key = splitLine[0];
+        key = stripSpecialChars(key);
+        return key;
+    }
+
+    private String stripSpecialChars(String string) {
+        Matcher matcher = Pattern.compile("[^a-zA-Z0-9]").matcher(string);
+        if (matcher.find()){
+            string = string.replaceAll(matcher.group(), "");
+        }
+        return string;
+    }
+
+    public String readOneLineValue(String filename, String searchTerm) throws IOException {
+        String line;
         BufferedReader bufferedReader = new BufferedReader(new FileReader(filename));
-        String line = bufferedReader.readLine();
-        while(line != null) {
+        do {
             line = bufferedReader.readLine();
-            if (StringUtils.startsWith(line, "\t" + searchTerm)){
+            if (StringUtils.startsWith(line, TAB_PREFIX + searchTerm + EQUAL_SUFFIX)){
                 break;
             }
-        }
+        } while(line != null);
         bufferedReader.close();
 
         if (line != null){
@@ -33,8 +93,10 @@ public class ParseCK2FileService {
     }
 
     private String extractValue(String line) {
-        String[] splitLine = StringUtils.split(line, "=");
-
-        return StringUtils.strip(splitLine[1], "\"\"");
+        String[] splitLine = StringUtils.split(line, EQUAL_SUFFIX);
+        String value = splitLine[1];
+        value = stripSpecialChars(value);
+        return value;
     }
+
 }
--- src/test/java/ParseCK2FileServiceTest.java
+++ src/test/java/ParseCK2FileServiceTest.java
@@ -1,7 +1,11 @@
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.nullValue;
@@ -23,23 +27,30 @@ public class ParseCK2FileServiceTest {
     }
 
     @Test
+    public void given_isValidSearchForPlayer_REAL_FILE_then_correctValueIsReturned() throws IOException {
+        Map<String, Map<String, String>> playerValues = parseCK2FileService.readMultiLineValue(REAL_FILE_FILENAME, "player");
+        assertThat(playerValues.get("player").get("id"), is("1613"));
+        assertThat(playerValues.get("player").get("type"), is("66"));
+    }
+
+    @Test
     public void given_isValidSearchTerm_REAL_FILE_then_correctValueIsReturned() throws IOException {
-        assertThat(parseCK2FileService.readFile(REAL_FILE_FILENAME, "date"), is("887.10.1"));
+        assertThat(parseCK2FileService.readOneLineValue(REAL_FILE_FILENAME, "date"), is("887.10.1"));
     }
 
     @Test
     public void given_isInvalidSearchTerm_REAL_FILE_then_nullIsReturned() throws IOException {
-        assertThat(parseCK2FileService.readFile(REAL_FILE_FILENAME, "blabla"), nullValue());
+        assertThat(parseCK2FileService.readOneLineValue(REAL_FILE_FILENAME, "blabla"), nullValue());
     }
 
     @Test
     public void given_isValidSearchTerm_then_correctValueIsReturned() throws IOException {
-        assertThat(parseCK2FileService.readFile(TEST_FILE_SIMPLE_FILENAME, "date"), is("887.10.1"));
+        assertThat(parseCK2FileService.readOneLineValue(TEST_FILE_SIMPLE_FILENAME, "date"), is("887.10.1"));
     }
 
     @Test
     public void given_isInvalidSearchTerm_then_nullIsReturned() throws IOException {
-        assertThat(parseCK2FileService.readFile(TEST_FILE_SIMPLE_FILENAME, "blabla"), nullValue());
+        assertThat(parseCK2FileService.readOneLineValue(TEST_FILE_SIMPLE_FILENAME, "blabla"), nullValue());
     }
 
 }
